c0ba1ae62aba28bc10c924540243f852e13db7d6
Albin Forsberg <foal21fs@student.ju.se>
2022-09-24 21:33:46 +0200

    Hatat djsflkjsf

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     distDir: "build",
-    images: {
-        unoptimized: true,
-    },
 };
 
 module.exports = {
     nextConfig,
+    images: {
+        unoptimized: true,
+    },
 };
