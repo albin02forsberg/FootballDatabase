a7877727409e457ccb89cdfa535c0bb691594f41
Albin Forsberg <foal21fs@student.ju.se>
2022-09-30 18:40:21 +0200

    Added desc

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     distDir: "build",
+    exportPathMap: async function(
+        defaultPathMap, { dev, dir, outDir, distDir, buildId }
+    ) {
+        return {
+            "/": { page: "/" },
+            "/about": { page: "/about" },
+        };
+    },
 };
 
 module.exports = {
