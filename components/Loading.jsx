e0b93dc64a29e6e08823e3064e17c76f212f9216
Albin Forsberg <foal21fs@student.ju.se>
2022-10-03 13:03:35 +0200

    improvements.

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -5,14 +5,18 @@ import React from "react";
 export default function Loading() {
   // Return loading screen
   return (
-    <section class="py-5">
-      <div class="container px-5 my-5">
-        <div class="row gx-5 justify-content-center">
-          <div class="col-lg-12">
-            <LinearProgress />
+    <header class="bg-dark py-5">
+      <div class="container px-5">
+        <div class="row gx-5 align-items-center justify-content-center">
+          <div class="col-lg-8 col-xl-7 col-xxl-6">
+            <div class="my-5 text-center text-xl-start">
+              <h1 class="display-5 fw-bolder text-white mb-2">
+                Laddar innehåll...
+              </h1>
+            </div>
           </div>
         </div>
       </div>
-    </section>
+    </header>
   );
 }
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -22,7 +22,8 @@ function Load() {
 
   useEffect(() => {
     const handleStart = (url) => url !== router.asPath && setLoading(true);
-    const handleComplete = (url) => url === router.asPath && setLoading(false);
+    const handleComplete = (url) =>
+      url === router.asPath && setTimeout(() => setLoading(false), 1000);
 
     router.events.on("routeChangeStart", handleStart);
     router.events.on("routeChangeComplete", handleComplete);
